87d4f07be8c8d08dff8f6ee6b4896e3e8c95cba4
lalit-logward <123081304+lalit-logward@users.noreply.github.com>
2023-11-12 22:43:40 +0530

    error class fix

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,5 +1,6 @@
 class ApiError extends Error {
     constructor(statusCode, message = 'Something went wrong', errors = [], stack) {
+        super(message)
         this.statusCode = statusCode;
         this.message = message;
         this.errors = errors;
